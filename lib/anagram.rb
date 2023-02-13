13555d2eca840e38a7f7cfec428b7d1d00becd35
sainamercy <sainamercy@gmail.com>
2023-02-13 14:45:50 +0300

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initialize word
+        @word = word
+    end
+    def match poss_anagrams
+        poss_anagrams.select do |w| 
+            w.chars.sort == @word.chars.sort
+        end
+    end 
+end    
+
+# listen = Anagram.new("listen")
+# puts listen.match ["enlists", "google", "inlets", "banana"]
+
